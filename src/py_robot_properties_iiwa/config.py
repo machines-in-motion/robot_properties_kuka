044a2847c0224dbfe274816e3f1390487b3437a0
Sebastien Kleff <sk8001@nyu.edu>
2019-12-13 14:56:51 -0500

    created pybullet wrapper for iiwa + cmakelists changes + added tennisball endeffector in urdf

--- src/py_robot_properties_iiwa/config.py
+++ src/py_robot_properties_iiwa/config.py
@@ -1,21 +1,58 @@
 import numpy as np
 from math import pi
 import rospkg
-from os.path import join
+from os.path import join, dirname
+from os import environ
 import pinocchio as se3
+from pinocchio.utils import zero
+from pinocchio.robot_wrapper import RobotWrapper
 
-
-class IiwaConfig:
+class IiwaConfig(object):
     robot_name = "iiwa"
 
+    @classmethod
+    def buildRobotWrapper(cls):
+        # Rebuild the robot wrapper instead of using the existing model to
+        # also load the visuals.
+        robot = RobotWrapper.BuildFromURDF(cls.urdf_path, cls.meshes_path)
+        robot.model.rotorInertia[6:] = cls.motor_inertia
+        robot.model.rotorGearRatio[6:] = cls.motor_gear_ration
+        return robot
+        
+    # Here we use the same urdf as for the quadruped but without the freeflyer.
     urdf_path = (
-        join(rospkg.RosPack().get_path("robot_properties_stuggihop"),
+        join(rospkg.RosPack().get_path("robot_properties_" + robot_name),
              "urdf",
-             "iiwa.urdf")
+             robot_name + ".urdf")
     )
 
+    meshes_path = [
+      dirname(rospkg.RosPack().get_path("robot_properties_" + robot_name))
+    ]
+
     yaml_path = (
-        join(rospkg.RosPack().get_path("robot_properties_stuggihop"),
+        join(rospkg.RosPack().get_path("robot_properties_" + robot_name),
              "config",
              "iiwa.yaml")
     )
+
+    # pinocchio model.
+    robot_model = se3.buildModelFromUrdf(urdf_path,
+                                         se3.JointModelFreeFlyer())
+    
+    # The inertia of a single blmc_motor.
+    motor_inertia = 0.0000045
+
+    # The motor gear ratio.
+    motor_gear_ration = 9.
+
+    robot_model.rotorInertia[6:] = motor_inertia
+    robot_model.rotorGearRatio[6:] = motor_gear_ration
+
+    mass = np.sum([i.mass for i in robot_model.inertias])
+
+    base_name = robot_model.frames[2].name
+
+    # Define the initial configuration (pos and vel?)
+    initial_configuration = se3.utils.zero(robot_model.nq) #[0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0.] #0.4, 0., 0., 0., 1.] + 4*[0., 0.8, -1.6]
+    initial_velocity = se3.utils.zero(robot_model.nv) # [0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0.] #(8 + 4 + 6)*[0,]
---
+++ src/py_robot_properties_iiwa/iiwawrapper.py
@@ -0,0 +1,101 @@
+import numpy as np
+import time
+import os
+import rospkg
+import pybullet as p
+import pinocchio as se3
+from py_pinocchio_bullet.wrapper import PinBulletWrapper
+from py_robot_properties_iiwa.config import IiwaConfig
+
+
+dt = 1e-3
+
+class IiwaRobot(PinBulletWrapper):
+    @staticmethod
+    def initPhysicsClient():
+        physicsClient = p.connect(p.GUI)
+        p.setGravity(0,0, -9.81)
+        p.setPhysicsEngineParameter(fixedTimeStep=dt, numSubSteps=1)
+        return physicsClient
+    
+    def __init__(self, physicsClient=None):
+        if physicsClient is None:
+            self.physicsClient = self.initPhysicsClient()
+
+        # Load the plain.
+        plain_urdf = (rospkg.RosPack().get_path("robot_properties_iiwa") +
+                      "/urdf/plane_with_restitution.urdf")
+        self.planeId = p.loadURDF(plain_urdf)
+
+        # Load the robot
+        robotStartPos = [0., 0, 0.0]
+        robotStartOrientation = p.getQuaternionFromEuler([0,0,0])
+
+        self.urdf_path = IiwaConfig.urdf_path
+        self.robotId = p.loadURDF(self.urdf_path, robotStartPos,
+            robotStartOrientation, flags=p.URDF_USE_INERTIA_FROM_FILE,
+            useFixedBase=True) # why not true?
+        p.getBasePositionAndOrientation(self.robotId)
+
+        # Create the robot wrapper in pinocchio.
+        package_dirs = [os.path.dirname(os.path.dirname(self.urdf_path)) + '/urdf']
+        self.pin_robot = IiwaConfig.buildRobotWrapper()
+
+        # Query all the joints.
+        num_joints = p.getNumJoints(self.robotId)
+
+        for ji in range(num_joints):
+            p.changeDynamics(self.robotId, ji, linearDamping=.04,
+                angularDamping=0.04, restitution=0.0, lateralFriction=0.5)
+
+        self.base_link_name = "base_link"
+        controlled_joints = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7']
+        self.joint_names = controlled_joints
+
+        # Creates the wrapper by calling the super.__init__.
+        super(IiwaRobot, self).__init__(self.robotId, self.pin_robot,
+            controlled_joints,
+            ['END'],
+            useFixedBase=True
+            )
+
+        
+    def forward_robot(self, q=None, dq=None):
+        if q is None:
+            q, dq = self.get_state()
+        elif dq is None:
+            raise ValueError('Need to provide q and dq or non of them.')
+        
+        self.pin_robot.forwardKinematics(q, dq)
+        self.pin_robot.computeJointJacobians(q)
+        self.pin_robot.framesForwardKinematics(q)
+        self.pin_robot.centroidalMomentum(q, dq)
+        
+        
+# if __name__ == "__main__":
+#     # Create a robot instance. This initializes the simulator as well.
+#     robot = IiwaRobot()
+#     tau = np.zeros(7)
+
+#     # Reset the robot to some initial state.
+#     q0 = np.matrix(IiwaConfig.initial_configuration).T
+#     dq0 = np.matrix(IiwaConfig.initial_velocity).T
+#     robot.reset_state(q0, dq0)
+
+
+#     # Run the simulator for 100 steps
+#     for i in range(230):
+#         # TODO: Implement a controller here.
+#         robot.send_joint_command(tau)
+        
+#         # Step the simulator.
+#         p.stepSimulation()
+#         time.sleep(0.1) # You can sleep here if you want to slow down the replay
+
+#     # Read the final state and forces after the stepping.
+#     q, dq = robot.get_state()
+#     active_eff, forces = robot.get_force()
+#     print('q', q)
+#     print('dq', dq)
+#     print('active eff', active_eff)
+#     print('forces', forces)
