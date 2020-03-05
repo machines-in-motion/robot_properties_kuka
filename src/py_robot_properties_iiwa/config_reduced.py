b57e8178b53302ccfe625c515931bf39007f6539
Sebastien Kleff <sk8001@nyu.edu>
2020-03-05 15:17:07 -0500

    renamed signals in iiwa.yaml and iiwa_wrench.yaml and updated package.xml

---
+++ src/py_robot_properties_iiwa/config_reduced.py
@@ -0,0 +1,45 @@
+import numpy as np
+from math import pi
+import rospkg
+from os.path import join, dirname
+from os import environ
+import pinocchio as se3
+from pinocchio.utils import zero
+from pinocchio.robot_wrapper import RobotWrapper
+
+class IiwaConfig(object):
+    robot_name = "iiwa_reduced"
+
+    @classmethod
+    def buildRobotWrapper(cls):
+        # Rebuild the robot wrapper instead of using the existing model to
+        # also load the visuals.
+        robot = RobotWrapper.BuildFromURDF(cls.urdf_path, cls.meshes_path)
+        return robot
+        
+    # Here we use the same urdf as for the quadruped but without the freeflyer.
+    urdf_path = (
+        join(rospkg.RosPack().get_path("robot_properties_" + robot_name),
+             "urdf",
+             robot_name + ".urdf")
+    )
+
+    meshes_path = [
+      dirname(rospkg.RosPack().get_path("robot_properties_" + robot_name))
+    ]
+
+    yaml_path = (
+        join(rospkg.RosPack().get_path("robot_properties_" + robot_name),
+             "config",
+             "iiwa.yaml")
+    )
+
+    # pinocchio model.
+    robot_model = se3.buildModelFromUrdf(urdf_path,
+                                         se3.JointModelFreeFlyer())
+
+    base_name = robot_model.frames[2].name
+
+    # Define the initial configuration (pos and vel?)
+    initial_configuration = se3.utils.zero(robot_model.nq) 
+    initial_velocity = se3.utils.zero(robot_model.nv) 
