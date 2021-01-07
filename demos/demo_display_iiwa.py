41e57e80863ff86c124991b1b066a79db42e7c53
Sebastien Kleff <skleff@akanagi.laas.fr>
2021-01-07 09:35:34 +0100

    cleaned up demo display gepetto gui

--- demos/demo_display_iiwa.py
+++ demos/demo_display_iiwa.py
@@ -11,58 +11,27 @@ All rights reserved.
 """
 
 
-def with_gepetto_gui_helper():
-    import time
-    from robot_properties_kuka.config import IiwaConfig
-    from gepetto_gui_helper.gepetto_gui_scene import GepettoGuiScene
-    from gepetto_gui_helper.robot_visual import RobotVisual
-    from gepetto_gui_helper.frame import Frame
+from robot_properties_kuka.config import IiwaConfig
+import numpy as np 
+import time 
 
-    gepetto_gui_scene = GepettoGuiScene("kuka_scene", "kuka_window")
-    config = IiwaConfig()
-    iiwa_visual = RobotVisual(
-        gepetto_gui_scene, "iiwa", config.urdf_path, config.meshes_path
-    )
-    iiwa_visual.display(config.q0)
-    # place the world frame
-    world_frame = Frame(gepetto_gui_scene)
+# Load the robot urdf.
+robot = IiwaConfig.buildRobotWrapper()
 
-    # # Example of moving the robot forward and updating the display every time.
-    # q = config.q0.copy()
-    # for i in range(10):
-    #     q[0] += 0.05
-    #     iiwa_visual.display(q)
-    #     time.sleep(0.2)
+# Setup the display (connection to gepetto viewer) and load the robot model.
+robot.initDisplay(loadModel=True)
 
+# Create a first initial position for the robot. Both legs are bent inwards.
+q = np.matrix(IiwaConfig.initial_configuration).T
 
-def with_pinocchio_wrapper():
-    import numpy as np
-    import pinocchio as se3
-    import time
-    import os
+# Display the configuration in the viewer.
+robot.display(q)
 
-    from robot_properties_kuka.config import IiwaConfig
-
-    # Load the robot urdf.
-    robot = IiwaConfig.buildRobotWrapper()
-
-    # Setup the display (connection to gepetto viewer) and load the robot model.
-    robot.initDisplay(loadModel=True)
-
-    # Create a first initial position for the robot. Both legs are bent inwards.
-    q = np.matrix(IiwaConfig.initial_configuration).T
-
-    # Display the configuration in the viewer.
+# Example of moving the robot forward and updating the display every time.
+for i in range(100):
+    print(i)
+    q[0] += 0.05
+    q[1] -= 0.01
+    q[2] += 0.05
     robot.display(q)
-
-    # # Example of moving the robot forward and updating the display every time.
-    # for i in range(10):
-    #     q[0] += 0.05
-    #     robot.display(q)
-    #     time.sleep(0.2)
-
-
-if __name__ == "__main__":
-
-    with_pinocchio_wrapper()
-    # with_gepetto_gui_helper()
+    time.sleep(0.1)
